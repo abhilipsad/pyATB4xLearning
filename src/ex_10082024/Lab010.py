fda8493c846bdcca30c2a3da0219b692036afecd
abhilipsad <dash.abhilipsa@gmail.com>
2024-08-21 22:17:36 +0530

    Lab002 adding

--- src/ex_10082024/Lab010.py
+++ src/ex_10082024/Lab010.py
@@ -1,12 +1,6 @@
-#Identifiers are the names used to identify a variable, function, class, module, or other objects.
-#They start with a letter (A-Z or a-z) or an underscore (_) followed by zero or more letters, underscores, and digits (0-9).
-#Python is case-sensitive, so myVariable and myvariable are two different identifiers.
-
-a = 65
-print(a)
-
-_=10
-print(_)
-
-_=11+1
-print(_)
+age = 65
+print(age)
+print(type(age))
+age = "pramod"
+print(age)
+print(type(age))
