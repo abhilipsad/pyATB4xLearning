5f9b490963e623a64814dec3feff081e933ac602
abhilipsad <dash.abhilipsa@gmail.com>
2024-08-23 20:20:19 +0530

    Lab0015 & lab0016 adding

---
+++ src/ex_13082024/Lab0015MaxNumber.py
@@ -0,0 +1,3 @@
+# To find the maximum number
+result = max(3, 4)
+print(result)
---
+++ src/ex_13082024/Lab0016Datatypes.py
@@ -0,0 +1,16 @@
+# Different datatypes
+# int -> -1 -2 333 123
+# float -> 1.234 2.78
+# boolean -> True, False
+# strings -> IIT,abc&we, test "" ''
+# Basic int, float,bool, str
+# Advance list,set,tuple,dict (in future)
+
+
+age = 34
+print(type(age))
+# <class 'int'>
+
+name = "pramod"
+print(type(name))
+# <class 'str'>
